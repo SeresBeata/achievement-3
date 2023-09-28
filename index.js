7c86de75acd251aa9711ed6682026dbd42114a99
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-09-28 10:04:17 +0200

    Require modules
    Require modules, like fs, path, Express and  Morgan.

---
+++ index.js
@@ -0,0 +1,5 @@
+//Require Express
+const express = require('express'),
+    morgan = require('morgan'), //Require Morgan
+    fs = require('fs'), // Require Node module fs
+    path = require('path'); // Require Node module path
