3b9b6d5a3c848d1b47c8c6c77cb57cc0a9fe7e83
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-09-28 10:06:47 +0200

    Declare variables
    Declare variables for encapsulating Express’s functionality and for server port.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express = require('express'),
     morgan = require('morgan'), //Require Morgan
     fs = require('fs'), // Require Node module fs
     path = require('path'); // Require Node module path
+//Declare variable that encapsulates Express’s functionality
+const app = express();
+//Server Port
+const port = 3000;
