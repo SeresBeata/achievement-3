6551b04f728ad95e091db673eaee00b237755d6a
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-10-18 16:22:44 +0200

    Create “/login” endpoint in auth.js
    Create “/login” endpoint for registered users that contains logic for authenticating users with basic HTTP authentication and generating a JWT token for authenticating future requests.

--- auth.js
+++ auth.js
@@ -6,3 +6,38 @@ const jwt = require('jsonwebtoken'),
     passport = require('passport');
 
 require('./passport'); // Local passport.js file
+
+//Generate the token
+let generateJWTToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.username, // The username, that is encoded in the JWT
+        expiresIn: '7d', // The token expire in 7 days
+        algorithm: 'HS256', // The algorithm used to “sign” or encode the values of the JWT
+    });
+};
+
+//Create “/login” endpoint for registered users
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        console.log('called');
+        passport.authenticate(
+            'local',
+            { session: false },
+            (error, user, info) => {
+                if (error || !user) {
+                    return res.status(400).json({
+                        message: 'Something is not right',
+                        user: user,
+                    });
+                }
+                req.login(user, { session: false }, (error) => {
+                    if (error) {
+                        res.send(error);
+                    }
+                    let token = generateJWTToken(user.toJSON());
+                    return res.json({ user, token }); //returns the token. Shorthand for res.json({ user: user, token: token })
+                });
+            }
+        )(req, res);
+    });
+};
